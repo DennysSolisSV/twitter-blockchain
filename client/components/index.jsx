0839fe29888e3e9ff351e719d84b0c77be8b3df4
Dennys Solis <dennyssolis@gmail.com>
2022-03-04 16:24:44 -0500

    widgets

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -1,4 +1,4 @@
 export {default as Sidebar} from './Sidebar';
-export {default as SidebarOption} from './SidebarOptionSidebarOption';
+export {default as SidebarOption} from './SidebarOption';
 export {default as Feed} from './Feed';
 export {default as Widgets} from './Widgets';
